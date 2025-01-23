a15f6674d333e5aa6e5d1e7dd2833bd6e6fa63a8
Enol <enolgr@gmail.com>
2025-01-23 10:47:34 +0100

    modal añadir vinilos

--- gestor.php
+++ gestor.php
@@ -3,11 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="./styles/gestor.css">
     <title>Vinyl Store Manager</title>
 </head>
 <body>
     <h1>Vinyl Store Manager</h1>
+
+    <div class="buscador">
+    <div class="search-container">
+    <input type="text" id="search-input" placeholder="Buscar vinilos">
+    <button>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search">
+            <circle cx="11" cy="11" r="8"></circle>
+            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+    </button>
+</div>
+<select name="publicados" id="publicados">
+  <option value="1">
+    publicado
+  </option>
+  <option value="0">
+    no publicado
+  </option>
+  </select>
+  <select name="genero" id="genero">
+   <option value="1">Rock</option>
+   <option value="2">Pop</option>
+  </select>
+
+    </div>
+
     <table class="vinyl-table">
         <thead>
             <tr>
@@ -23,6 +50,54 @@
         <tbody>
         </tbody>
     </table>
+
+    <div id="boton-add">
+      +
+    </div>
+
+    <div id="modal-add">
+    <div class="modal-content">
+        <button class="close-button">&times;</button>
+        <form class="add-album">
+    <label for="titulo">Título del álbum</label>
+    <input name="titulo" type="text" placeholder="Escribe el título del álbum" required>
+
+    <label for="artista">Artista</label>
+    <input name="artista" type="text" placeholder="Escribe el nombre del artista" required>
+
+    <label for="genero">Género</label>
+    <select name="genero" id="genero" required>
+        <option value="">Selecciona un género</option>
+        <option value="1">Rock</option>
+        <option value="2">Pop</option>
+        <option value="3">Indie</option>
+        <option value="4">Electrónica</option>
+        <option value="5">Jazz</option>
+        <option value="6">Hip Hop</option>
+        <option value="7">Reggae</option>
+        <option value="8">Flamenco</option>
+        <option value="9">Folk</option>
+        <option value="10">Experimental</option>
+        <option value="11">Punk</option>
+        <option value="12">Trap</option>
+        <option value="13">Soul</option>
+        <option value="14">R&B</option>
+        <option value="15">Fusión</option>
+    </select>
+
+    <label for="descripcion">Descripción</label>
+    <input type="text" name="descripcion" placeholder="Añade una descripción" required>
+
+    <label for="imagen">Subir imagen</label>
+    <input type="file" name="imagen" id="imagen" accept="image/*" required>
+
+    <button type="submit">Añadir Álbum</button>
+</form>
+
+    </div>
+</div>
+
+
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="./js/mostrar-tabla.js"></script>
 </body>
--- js/mostrar-tabla.js
+++ js/mostrar-tabla.js
@@ -9,15 +9,18 @@ $(document).ready(function () {
                     let filas = "";
                     response.tabla_vinilos.forEach(function (vinilo) {
                         let description = vinilo.description || "Descripción no disponible";
+                        let publicadoCheckbox = vinilo.published == 1
+                            ? '<input type="checkbox" class="publicado-checkbox" checked disabled>'
+                            : '<input type="checkbox" class="publicado-checkbox" disabled>';
                         filas += `
-                            <tr class="vinyl-item">
+                            <tr class="vinyl-item" data-publicado="${vinilo.published}">
                                 <td><img class="item img-disco" src="${vinilo.image}" alt="${vinilo.title}"></td>
                                 <td class="item nombre-disco">${vinilo.title}</td>
                                 <td class="item precio-disco">${vinilo.price} &euro;</td>
                                 <td class="item descripcion-disco">${description}</td>
                                 <td class="item autor-disco">${vinilo.artist}</td>
                                 <td class="item genero-disco">${vinilo.genre}</td>
-                                <td class="item publicado-disco">${vinilo.published}</td>
+                                <td class="item publicado-disco">${publicadoCheckbox}</td>
                             </tr>`;
                     });
                     $(".vinyl-table tbody").html(filas);
@@ -32,7 +35,45 @@ $(document).ready(function () {
         });
     }
 
+    function filtrarPublicado() {
+        const publicado = $("#publicados").val();
+        $(".vinyl-item").each(function () {
+            const filaPublicado = $(this).data("publicado").toString();
+            if (publicado === "all" || filaPublicado === publicado) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    }
+
     cargarTabla();
+
+    $("#search-input").on("keyup", function () {
+        const query = $(this).val().toLowerCase();
+        $(".vinyl-item").each(function () {
+            const textoFila = $(this).text().toLowerCase();
+            if (textoFila.includes(query)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+
+    $("#publicados").on("change", function () {
+        filtrarPublicado();
+    });
+
+    // Abre la modal al hacer clic en el botón
+    $("#boton-add").on("click", function () {
+        $("#modal-add").addClass("active");
+    });
+
+    // Cierra la modal al hacer clic en el botón de cerrar
+    $(document).on("click", "#modal-add .close-button", function () {
+        $("#modal-add").removeClass("active");
+    });
 
     $(document).ajaxComplete(function () {
         $(".vinyl-table").css({
