51873a51168dbf586dc95d371ba21ae55be905fe
Enol <enolgr@gmail.com>
2025-01-23 09:07:02 +0100

    creando el front gestor de discos

---
+++ gestor.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/gestor.css">
+    <title>Vinyl Store Manager</title>
+</head>
+<body>
+    <h1>Vinyl Store Manager</h1>
+    <table class="vinyl-table">
+  <thead>
+    <tr>
+      <th>Imagen</th>
+      <th class="nombre-disco">Nombre</th>
+      <th class="precio-disco">Precio</th>
+      <th class="descripcion-disco">Descripción</th>
+      <th class="autor-disco">Autor</th>
+      <th class="genero-disco">Género</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="vinyl-item">
+      <td><img class="item img-disco" src="https://i.scdn.co/image/ab67616d0000b273a408c78e231f716383a58eb3" alt="Disco"></td>
+      <td class="item nombre-disco">Nombredeldisco</td>
+      <td class="item precio-disco">precio</td>
+      <td class="item descripcion-disco">descripcion</td>
+      <td class="item autor-disco">autor</td>
+      <td class="item genero-disco">genero</td>
+    </tr>
+    <!-- Puedes agregar más filas para otros discos -->
+  </tbody>
+</table>
+</body>
+</html>
